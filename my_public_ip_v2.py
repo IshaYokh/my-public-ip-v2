1086c47f83e54a5d819d7f091862cd9e7e56998a
IshaYokh <>
2021-01-14 20:06:22 +1100

    Added some skeleton code

---
+++ my_public_ip_v2.py
@@ -0,0 +1,52 @@
+# Author: @IshaYokh
+# Code repo: https://github.com/IshaYokh/my-public-ip-v2
+
+import subprocess
+import sys
+import os
+import argparse
+import time
+import socket
+import smtplib
+from email.message import EmailMessage
+from twilio.rest import Client
+import sqlite3
+
+
+def main():
+    pass
+
+
+def run_config():
+    pass
+
+
+def take_args():
+    pass
+
+
+def get_ip():
+    pass
+
+
+def send_notification():
+    pass
+
+
+def add_to_db():
+    pass
+
+
+def check_db():
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("[!] Program interrupted by user - Exiting")
+
+
+# Author: @IshaYokh
+# Code repo: https://github.com/IshaYokh/my-public-ip-v2
