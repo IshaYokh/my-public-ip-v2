190f9ef1298d0f885503c4ec8ce028a20b7d36a1
IshaYokh <>
2021-01-14 21:14:33 +1100

    Implemented get_ip, init_sqlite, close_db, add_to_db and check_db functions

--- my_public_ip_v2.py
+++ my_public_ip_v2.py
@@ -31,20 +31,55 @@ def take_args():
     return args
 
 
+# Gets public ip address using ipify.org API
 def get_ip():
-    pass
+    return requests.get("https://api.ipify.org").text
 
 
 def send_notification():
     pass
 
 
-def add_to_db():
-    pass
+# Creates an sqlite object, cursor and db table
+def init_sqlite():
+    sql_connection = sqlite3.connect("ip_addresses.db")
+    sql_cursor = self.sql_connection.cursor()
+    sql.cursor.execute(""" CREATE TABLE ip_addresses (ip_address string) """)
+
+    return sql_connection, sql_cursor
 
 
-def check_db():
-    pass
+# Closes connection to local database
+def close_db():
+    sql_connection, sql_cursor = init_sqlite()
+    sql_connection.close()
+
+
+# Adds data to local database
+def add_to_db(ip_address):
+    if(check_db(ip_address)):
+        return
+
+    # Adding given ip address to local database if it's not already stored
+    command = """ INSERT INTO ip_addresses (ip_address) VALUES ("{ip_address}") """.format(ip_address=ip_address)
+    sql_connection, sql_cursor = init_sqlite()
+    sql_cursor.execute(command)
+    sql_connection.commit()
+
+
+# Checks if specific data exist in the local database
+def check_db(ip_address):
+    # Sending query to local database to check if the given ip address is already stored
+    query = """ SELECT ip_address FROM ip_addresses WHERE ip_address = "{ip_address}" """.format(ip_address=ip_address)
+    sql_connection, sql_cursor = init_sqlite()
+    query_return = sql_cursor.execute(query)
+    query_return = query_return.fetch_all()
+    sql_connection.commit()
+
+    if(query_return):
+        return True
+
+    return False
 
 
 if __name__ == "__main__":
